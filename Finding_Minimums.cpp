ce5a72783fbde38fe6fbeeed880ba7ef3ff5c607
Somaya Hussien <117348473+aptx04869@users.noreply.github.com>
2024-03-03 00:39:25 +0200

    CodeForces

---
+++ Finding_Minimums.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, K;
+    long long defmin = 1000000000, min, num;
+
+    min = defmin;
+
+    cin >> N >> K;
+
+        for (int i = 1; i <= N; i++){
+
+            cin >> num;
+            if( num < min ){
+                min = num;
+            }
+            if (i % K == 0){
+                cout << min << " ";
+                min = defmin;
+            }
+            else if(i == N){
+                cout << min;
+            }
+        }
+
+    return 0;
+}
